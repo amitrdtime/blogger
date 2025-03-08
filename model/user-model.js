232272113d640c97336abf2e0bd1bbcae97139b3
Amit <amit.deshmukh1014@gmail.com>
2025-03-08 14:51:30 +0530

    Made changes

--- model/user-model.js
+++ model/user-model.js
@@ -1,4 +1,6 @@
 const Users = require('../schemas/users-schema');
+const md5 = require("md5");
+const jwt = require("jsonwebtoken");
 
 const getUserData = async () => {
   try {
@@ -13,6 +15,9 @@ const getUserData = async () => {
 
 const addUser = async (userData) => {
   try {
+    console.log(userData);
+    userData.password = md5(userData.password);
+    console.log(userData);
     const insertResult = await Users.insertMany(userData);
     console.log(insertResult);
     return insertResult;
@@ -27,17 +32,20 @@ const loginUserdata = async (userData) => {
         // Hash the input password using MD5
         const {email, password} = userData;
         const hashedPassword = md5(password);
+        console.log('hashpassword', hashedPassword);
 
         // Find user with email and hashed password
+        // SELECT * FROM user WHERE email=${email} AND password=${hashedPassword}
         const user = await Users.findOne({ email, password: hashedPassword });
+        console.log(user);
         if (!user) {
             return { success: false, message: 'Invalid email or password' };
         }
 
         // Generate JWT token
         const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-        return { success: true, message: 'Login successful', token };
+        console.log(token)
+        return { success: true, message: 'Login successful', token, user };
     } catch (error) {
         console.error(error);
         return { success: false, message: 'Server error' };
